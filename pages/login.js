e70a6025d390d2267d55ff031129e0f21eba5392
RikeshThapa <rthapa@u.rochester.edu>
2022-06-29 19:03:11 -0700

    login is complete; turns out with magic, both login and signup go through same endpoint; so created signup page, empty

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Router from 'next/router'
+import Link from 'next/link'
 
 import { useUser } from '../lib/hooks'
 import { Magic } from 'magic-sdk'
@@ -49,21 +50,27 @@ const Login = () => {
 
     return (
         <div className='relative flex h-screen items-center bg-gradient-to-br from-nft-purple to-nft-cotton p-5 '>
-          <div className="grid grid-cols-1 grid-rows-5 bg-white p-5 h-full rounded-md w-full">
-              <div className='text-3xl row-span-1'><BackArrow/></div>
-              <div className='row-span-4'>
-                <div className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 text-6xl text-center">
-                  🤖
+          <div className='p-5 h-full rounded-md w-full bg-white grid grid-cols-1 divide-y divide-black'>
+            <div className="grid grid-cols-1 grid-rows-5">
+                <div className='text-3xl row-span-1'><BackArrow/></div>
+                <div className='row-span-4'>
+                  <div className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 text-6xl text-center">
+                    🤖
+                  </div>
+                  <div className='text-3xl font-bold text-center p-2'>
+                    Let's sign you in.
+                  </div>
+                  <div className='text-xl font-light text-center p-1'>
+                    Welcome back. We've missed you!
+                  </div>
+                  <LoginForm errorMessage={errorMsg} onSubmit={handleSubmit} />
                 </div>
-                <div className='text-3xl font-bold text-center p-2'>
-                  Let's sign you in.
-                </div>
-                <div className='text-xl font-light text-center p-1'>
-                  Welcome back. We've missed you!
-                </div>
-                <LoginForm errorMessage={errorMsg} onSubmit={handleSubmit} />
-              </div>
+            </div>
+            <div className='SignUp flex place-content-center p-4'>
+              Don't have an account? <Link href='/signup' ><div className='underline text-blue-600 pl-2'>Register Now</div></Link>
+            </div>
           </div>
+          
         </div>
     )
 }
---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+import Profile from "./profile"
+
+
+const SignUp = () => {
+    return (
+        <div>
+            Sign Up
+        </div>
+    )
+}
+
+
+export default SignUp
